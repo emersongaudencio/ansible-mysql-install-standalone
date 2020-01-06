d395aa49d435e05d24f022d1d4a36ee1012e1a3b
Emerson Gaudencio <emerson.fender@gmail.com>
2020-01-06 01:20:42 +0000

    Beta version 1.0.1

--- ansible/run_mysql_install.sh
+++ ansible/run_mysql_install.sh
@@ -6,12 +6,11 @@ export ANSIBLE_CONFIG=$SCRIPT_PATH/ansible.cfg
 
 cd $SCRIPT_PATH
 
-VAR_HOST="dbtest02"
-VAR_SSH_USER="master"
-VAR_MYSQL_VERSION="57"
+VAR_HOST="$1"
+VAR_MYSQL_VERSION="$2"
 
 ### Ping host ####
-ansible -i $SCRIPT_PATH/hosts -m ping $VAR_HOST -u $VAR_SSH_USER --ask-pass --become -v
+ansible -i $SCRIPT_PATH/hosts -m ping $VAR_HOST -v
 
 ### mysql install ####
-ansible-playbook -v -i $SCRIPT_PATH/hosts -e "{mysql_version: '$VAR_MYSQL_VERSION'}" $SCRIPT_PATH/playbook/mysql_install.yml -l $VAR_HOST  -u $VAR_SSH_USER --ask-pass --become
+ansible-playbook -v -i $SCRIPT_PATH/hosts -e "{mysql_version: '$VAR_MYSQL_VERSION'}" $SCRIPT_PATH/playbook/mysql_install.yml -l $VAR_HOST
